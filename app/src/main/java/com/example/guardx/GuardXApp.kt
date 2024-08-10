a8410bd1303ebf1ef92e7f5b93d77fd9dd5a709e
Sanjana C K <sanjanack874@gmail.com>
2024-08-10 14:00:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/guardx/GuardXApp.kt
@@ -0,0 +1,20 @@
+package com.example.guardx
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.guardx.ui.theme.HomeScreen
+import com.example.guardx.ui.theme.LoginScreen
+
+@Composable
+fun GuardXApp() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "login") {
+        composable("login") { LoginScreen(onLoginSuccess = { navController.navigate("home") }) }
+        composable("home") { HomeScreen(navController) }
+//        composable("first_aid") { FirstAidScreen() }
+//        composable("be_volunteer") { BeVolunteerScreen() }
+        // Add more composable destinations for other pages
+    }
+}
